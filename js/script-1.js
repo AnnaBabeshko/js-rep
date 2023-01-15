ad038a1d579df77d582dd8d1588f853d91c27394
Анна Бабешко <slashur@ukr.net>
2023-01-15 22:45:20 +0200

    zzz

---
+++ js/script-1.js
@@ -0,0 +1,7 @@
+const a = 'переменная "a" в script-1.js'
+
+console.log(a);
+
+const b = 'переменная "b" в script-1.js';
+
+console.log(b);
---
+++ js/script-2.js
@@ -0,0 +1,7 @@
+const a = 'переменная "a" в script-2.js'
+
+console.log(a);
+
+const c = 'переменная "c" в script-2.js';
+
+console.log(c);
