ae3da480997ad55419a54ac108bc3242b2aae068
Анна Бабешко <slashur@ukr.net>
2023-01-18 20:47:17 +0200

    vvv

--- js/script-1.js
+++ js/script-1.js
@@ -5,3 +5,19 @@ console.log(a);
 const b = 'переменная "b" в script-1.js';
 
 console.log(b);
+
+
+function formatMessage(message, maxLength) {
+  let result;
+  // Change code below this line
+  if (message.length <= maxLength) {
+    return message;
+  } 
+  else {
+    message.slice (0, maxLength) + '...';
+    return maxLength;
+  }
+  // Change code above this line
+  return result;
+}
+
