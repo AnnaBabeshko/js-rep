a62da9cf1b8adbf1db5c7e3ddba732c152dc5bba
Анна Бабешко <slashur@ukr.net>
2023-01-21 22:51:54 +0200

    ppp

--- js/script-1.js
+++ js/script-1.js
@@ -7,17 +7,14 @@ const b = 'переменная "b" в script-1.js';
 console.log(b);
 
 
-function formatMessage(message, maxLength) {
-  let result;
-  // Change code below this line
-  if (message.length <= maxLength) {
-    return message;
-  } 
-  else {
-    message.slice (0, maxLength) + '...';
-    return maxLength;
-  }
-  // Change code above this line
-  return result;
+function calculateTotal(number) {
+ // Change code below this line
+ 
+ let sum = 0;
+
+ for (let i = 0; i <= number; i += 1) {
+  sum += i;
 }
 
+// Change code above this line
+}
