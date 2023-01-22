ef309fc67e2be5bda4dccce18416ee04d7caae7b
Анна Бабешко <slashur@ukr.net>
2023-01-22 20:39:18 +0200

    nnn

--- js/script-1.js
+++ js/script-1.js
@@ -7,14 +7,4 @@ const b = 'переменная "b" в script-1.js';
 console.log(b);
 
 
-function calculateTotal(number) {
- // Change code below this line
- 
- let sum = 0;
 
- for (let i = 0; i <= number; i += 1) {
-  sum += i;
-}
-
-// Change code above this line
-}
